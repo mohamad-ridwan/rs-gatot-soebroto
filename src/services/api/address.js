b00c6ba91fc32423493ca8e1e5f26c8c0dabaff6
mohamad-ridwan <mr643062@gmail.com>
2022-12-07 18:10:16 +0700

    ridwan1 change api with railway.app

--- src/services/api/address.js
+++ src/services/api/address.js
@@ -1,3 +1,4 @@
-const address = 'https://rs-gatot-soebroto.herokuapp.com'
+// const address = 'https://rs-gatot-soebroto.herokuapp.com'
+const address = 'https://web-production-391a.up.railway.app'
 
 export default address
